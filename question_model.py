aeca6acc3f169e505e800ad1a5e7b47902c80f36
mzxz <mohamed.abaker110@gmail.com>
2022-09-19 21:19:10 +0000

    create question class in quesition model

---
+++ question_model.py
@@ -0,0 +1,7 @@
+class Question:
+    """
+    Question class has question text attributes and answer
+    """
+    def __init__(self, question_text, question_aswer):
+        self.text = question_text
+        self.answer = question_aswer
