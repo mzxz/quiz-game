6ffde2ba9e91976cf91c7d5b0aa8f92e697f8082
mzxz <mohamed.abaker110@gmail.com>
2022-09-19 22:44:45 +0000

    import question data and question to run.py

--- data.py
+++ data.py
@@ -9,24 +9,35 @@ question_data = [
      "question": "Gumbo is a stew that originated in Louisiana.",
      "correct_answer": "True", "incorrect_answers": ["False"]},
     {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
-     "question": "It is automatically considered entrapment in the United States"
-                 " if the police sell you illegal substances without revealing themselves.",
+     "question": "It is automatically considered entrapment in "
+                 "the United States if the police sell you"
+                 " illegal substances without revealing themselves.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean",
+     "difficulty": "easy",
+     "question": "A scientific study on peanuts in bars found"
+                 " traces of over 100 unique specimens of urine.",
      "correct_answer": "False", "incorrect_answers": ["True"]},
     {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
-     "question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+     "question": "Bulls are attracted to the color red.",
      "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
-     "question": "Bulls are attracted to the color red.", "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
-     "question": "The color orange is named after the fruit.", "correct_answer": "True",
+    {"category": "General Knowledge", "type": "boolean",
+     "difficulty": "easy",
+     "question": "The color orange is named after the fruit.",
+     "correct_answer": "True",
      "incorrect_answers": ["False"]},
     {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
-     "question": "In 2010, Twitter and the United States Library of Congress partnered together"
+     "question": "In 2010, Twitter and the United States Library of Congress"
+                 " partnered together"
                  " to archive every tweet by American citizens.",
      "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy", "question": "Pluto is a planet.",
+    {"category": "General Knowledge", "type": "boolean",  
+     "difficulty": "easy", "question": "Pluto is a planet.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", 
+     "difficulty": "easy",
+     "question": "Adolf Hitler was born in Australia. ", 
      "correct_answer": "False", "incorrect_answers": ["True"]},
     {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
-     "question": "Adolf Hitler was born in Australia. ", "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
-     "question": "Ping-Pong originated in England", "correct_answer": "True", "incorrect_answers": ["False"]}]
+     "question": "Ping-Pong originated in England", 
+     "correct_answer": "True", "incorrect_answers": ["False"]}]
