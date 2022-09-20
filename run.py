53cef611c75793122e2aeb2076426b3a94867319
mzxz <mohamed.abaker110@gmail.com>
2022-09-20 07:25:13 +0000

    added opthion for play again

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import sys
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
@@ -10,12 +11,24 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-quiz = QuizBrain(question_bank)
 
-try:
-    while quiz.still_has_questions:
-        quiz.next_question()
-except IndexError:
-    print("The end")
-print("You have completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+def play():
+
+    quiz = QuizBrain(question_bank)
+    try:
+        while quiz.still_has_questions:
+            quiz.next_question()
+    except IndexError:
+        print("The end")
+        print("You have completed the quiz")
+        print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+        print("\n")
+        again = input("Do you want to play again(type y or n for yes or no): ")
+        while again == "y":
+            print("\n")
+            play()
+        else:
+            sys.exit()
+
+
+play()
