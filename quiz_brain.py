2ce4fce25df75b76d5015f9ecc4fddc30c2265a9
mzxz <mohamed.abaker110@gmail.com>
2022-09-20 04:44:26 +0000

    added still has question and next question methods' functionality

--- quiz_brain.py
+++ quiz_brain.py
@@ -8,10 +8,15 @@ class QuizBrain:
         self.question_list = q_list
 
     def still_has_questions(self):
-        pass
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
-        pass
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(
+            f"Q.{self.question_number}: {current_question.text} (True/False): "
+        ).lower()
+        self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
         pass
